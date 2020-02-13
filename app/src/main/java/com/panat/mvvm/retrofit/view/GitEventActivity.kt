fe452b69c5016d0b7127d3795d57ee7906dca5b3
M416 <chanon_w@cmu.ac.th>
2020-02-13 16:14:17 +0700

    rename

--- app/src/main/java/com/panat/mvvm/retrofit/view/GitEventActivity.kt
+++ app/src/main/java/com/panat/mvvm/retrofit/view/GitEventActivity.kt
@@ -6,7 +6,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.panat.mvvm.retrofit.R
 import com.panat.mvvm.retrofit.adapter.GitEventsAdapter
 import com.panat.mvvm.retrofit.viewModel.GitEventActivityViewModel
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_git_event.*
 import org.koin.android.ext.android.inject
 
 class GitEventActivity : AppCompatActivity() {
@@ -14,7 +14,7 @@ class GitEventActivity : AppCompatActivity() {
     private val viewModel: GitEventActivityViewModel by inject()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_git_event)
         val adapter = GitEventsAdapter(this)
         rvEvent.layoutManager = LinearLayoutManager(this)
         rvEvent.adapter = adapter
